1399276dcea0b77b8535fa01bd92c14d36a5573b
Giovanni <giovannidelgado@iMac-de-Giovanni.local>
2019-08-02 12:09:59 -0500

    App refactoring for structure

--- index.js
+++ index.js
@@ -5,36 +5,9 @@ var fs = require('fs')
 var path = require('path')
 var _ = require('lodash')
 var engines = require('consolidate')
+var helpers = require('./helpers')
 
 var bodyParser = require('body-parser')
-
-function getUserFilePath(username) {
-  return path.join(__dirname, 'users', username) + '.json'
-}
-
-function getUser(username) {
-  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}))
-  user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-  _.keys(user.location).forEach(function (key) {
-    user.location[key] = _.startCase(user.location[key])
-  })
-  return user
-}
-
-function saveUser (username, data) {
-  var fp = getUserFilePath(username)
-  fs.unlinkSync(fp) // delete the file
-  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'})
-}
-
-/* fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
-  if (err) throw err
-
-  JSON.parse(data).forEach(function(user) {
-    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-    user.push(user)
-  })
-}) */
 
 app.engine('hbs', engines.handlebars)
 
@@ -48,62 +21,42 @@ app.get('/favicon.ico', function (req, res) {
   res.end()
 })
 
-/* app.get('', function (req, res) {
-  var buffer = ''
+/* function getUserFilePath(username) {
+  return path.join(__dirname, 'users', username) + '.json'
+} */
 
-  users.forEach(function (user) {
-    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>'
-  })
-  res.send(buffer)
-}) */
-
-//app.get(/big.*/, function(req, res, next) {
-//  console.log('BIG USER ACCESS')
-//  next()
-//})
-
-//app.get(/.*dog.*/, function (req, res, next) {
-//  console.log('DOGS DO WOOF')
-//  next()
-//})
-
-app.get('/', function (req, res) {
-  // res.render('index', {users: users})
+app.get('/', function(req, res) {
   var users = []
-  fs.readdir('users', function (req, files) {
+  fs.readdir('users', function (err, files) {
+    if (err) throw err
     files.forEach(function (file) {
       fs.readFile(path.join(__dirname, 'users', file), {encoding: 'utf8'}, function (err, data) {
+        if (err) throw err
         var user = JSON.parse(data)
-        user.name.full = _.startCase(user.name.first +  ' ' + user.name.last)
-        user.push(user)
+        user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+        users.push(user)
         if (users.length === files.length) res.render('index', {users: users})
       })
     })
   })
 })
 
-app.get('/:username', function (req, res) {
-  var username = req.params.username
-  var user = getUser(username)
-  res.render('user',  {
-    user: user,
-    address: user.location     
-  })
+app.get('*.json', function (req, res) {
+  res.download('./users/', req.path, '')
 })
 
-app.put('/:username', function(req, res) {
+app.get('/data/:username', function (req, res) {
   var username = req.params.username
-  var user = getUser(username)
-  user.location = req.body
-  saveUser(username, user)
-  res.end()
+  var user = helpers.getUser(username)
+  res.json(user)
 })
 
-app.delete('/:username', function(req, res) {
-  var fp = getUserFilePath(req.params.username)
-  fs.unlinkSync(fp) // delete the file
-  res.sendStatus(200)
+app.get('/error/:username', function (req, res) {
+  res.status(404).send('No user named' + req.params.username + ' found')
 })
+
+var userRouter = require('./username')
+app.use('/:username', userRouter)
 
 var server = app.listen(3000, function() {
   console.log('Server running at http://localhost:' + server.address().port)
